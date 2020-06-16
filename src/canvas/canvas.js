1cdaf33d3f4abafe9f87865118b967babdf201d7
Sam <sjstorment22@gmail.com>
2020-06-16 15:39:31 -0500

    Changed fill to use queue (BFS). No immediately noticeable difference.

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -1,7 +1,6 @@
 import { Rectangle, Ellipse, Polygon } from './shape.js';
 import { getMousePosition } from './util.js';
-import Color from './color.js'
-import Fill from './fill.js';
+import { floodFill } from './fill.js';
 
 // CANVAS - this is where drawings will show up
 let canvas = document.querySelector('#canvas');
@@ -26,9 +25,10 @@ function initCanvas() {
 
 initCanvas();
 
-let fill = new Fill();
 
-let strokeWeight = 15;
+
+let showHover = true;
+// let strokeWeight = 15;
 // checks if the mouse is down (we are painting when the mouse is clicked)
 let painting = false;
 let mouseDown = false;
@@ -43,15 +43,9 @@ let lineCheck = document.querySelector('#line-check');
 let radialCheck = document.querySelector('#radial-check');
 let circleCheck = document.querySelector('#circle-check');
 let fillCheck = document.querySelector('#fill-check');
-fillCheck.addEventListener('click', () => {
-    // set the slider size to 0 so the hover cursor doesn't block our view
-    strokeSlider.value = 1;
-});
+fillCheck.addEventListener('click', () => { showHover = false; });
 let pickerCheck = document.querySelector('#picker-check');
-pickerCheck.addEventListener('change', () => {
-    // set the slider size to 0 so the hover cursor doesn't block our view
-    strokeSlider.value = 1;
-});
+pickerCheck.addEventListener('click', () => { showHover = false; });
 let clearButton = document.querySelector('#canvas-clear');
 clearButton.addEventListener('click', clearCanvas);
 let strokeColor = document.querySelector('#stroke-color');
@@ -62,7 +56,7 @@ let strokeSlider = document.querySelector('#stroke-slider');
 let checkBoxes = document.querySelectorAll('.default-hover');
 checkBoxes.forEach(element => {
     element.addEventListener('click', () => {
-        strokeSlider.value = strokeWeight;
+        showHover = true;
     });
 });
 
@@ -105,12 +99,7 @@ function startFill(event) {
     let { mouseX, mouseY } = getMousePosition(event);
     // fill.floodFillRecurse(mouseX, mouseY, startClr, fillClr, canvas, context);
 
-    let color = new Color();
-
-    let { red, green, blue, alpha } = color.hexToRGB(fillColor.value);
-    let fillClr = [ red, green, blue, alpha ];
-
-    fill.floodFill(context, mouseX, mouseY, fillClr, 128);
+    floodFill(context, mouseX, mouseY, fillColor.value, 128);
 
     context.beginPath();
     // clearPreview();
@@ -128,7 +117,7 @@ function startPicker(event) {
 function finishPicker(event) {
     // return the paint mode to brush, this should actually go back to the last selected mode
     brushCheck.checked = true;
-    // change the brush size back to its value before clicking the picker
+    showHover = true;
 }
 
 function startEllipse(event) {
@@ -293,36 +282,33 @@ function finishRadialLine(event) {
 // THIS SHIT IS UGLY AND BAD. How do we do it better???
 // MAIN START, DRAW, FINISH
 function start(event) {
-    if (rectCheck.checked)          { startRect(event); }
+    if (brushCheck.checked)         { startBrush(event); }
+    else if (rectCheck.checked)     { startRect(event); }
     else if (lineCheck.checked)     { startLine(event); }
     else if (radialCheck.checked)   { startRadialLine(event); }
     else if (circleCheck.checked)   { startEllipse(event); }
     else if (fillCheck.checked)     { startFill(event); }
     else if (pickerCheck.checked)   { startPicker(event); }
-    else                            { startBrush(event); }
 }
 
 function draw(event) {
     // if we are just hovering over the canvas without holding the mouse, show the hover
     if (!mouseDown)                 { showHoverCursor(event); }
 
-    if (rectCheck.checked)          { drawRect(event); } 
+    if (brushCheck.checked)         { drawBrush(event); }
+    else if (rectCheck.checked)     { drawRect(event); } 
     else if (lineCheck.checked)     { drawLine(event); }
     else if (radialCheck.checked)   { drawRadialLine(event); }
     else if (circleCheck.checked)   { drawEllipse(event); }
-    else if (fillCheck.checked)     {  }
-    else if (pickerCheck.checked)   {  }
-    else                            { drawBrush(event); }
 }
 
 function finish(event) {
-    if (rectCheck.checked)          { finishRect(event); } 
+    if (brushCheck.checked)         { finishBrush(event); }
+    else if (rectCheck.checked)     { finishRect(event); } 
     else if (lineCheck.checked)     { finishLine(event); }
     else if (radialCheck.checked)   { finishRadialLine(event); }
     else if (circleCheck.checked)   { finishEllipse(event); }
-    else if (fillCheck.checked)     {  }
     else if (pickerCheck.checked)   { finishPicker(event); }
-    else                            { finishBrush(event); }
 }
 
 // EVENT LISTENERS
@@ -386,8 +372,10 @@ function clearPreview() {
 }
 
 function showHoverCursor(event) {
-    let { mouseX, mouseY } = getMousePosition(event);
 
+    if (!showHover) { return; }
+
+    let { mouseX, mouseY } = getMousePosition(event);
     // clear the preview canvas anytime we move, but draw right after
     clearPreview();
 
--- src/canvas/fill.js
+++ src/canvas/fill.js
@@ -1,123 +1,89 @@
-import Color from './color.js';
+import { hexToRGB } from './color.js';
 import Queue from '../data-structures/queue.js';
 
+// Iterative fill
+export function floodFill(ctx, x, y, hexFillColor, range = 1) {
 
-let color = new Color();
+    // fillColor needs to be converted to RGB since we get it from color input as a hex val
+    let fillColor = hexToRGB(hexFillColor);
 
-export default class Fill {
+    // read the pixels in the canvas
+    const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    
+    // flags for if we visited a pixel already
+    const visited = new Uint8Array(imageData.width, imageData.height);
+    
+    // get the color we're filling
+    const targetColor = getPixel(imageData, x, y);
 
-    // startX, startY are the mosue coordinates where the paint bucket click started
-    // Recursive flood fill often times causes a queue overflow
-    floodFillRecurse(startX, startY, startColor, fillColor, canvas, context) {
-        // BASE CASE - if X or Y are outside of the canvas, return
-        if (startX < 0 || startX > canvas.width || startY < 0 || startY > canvas.height) { return; }
-        
-        let pixelColor = this.getPixelColor(startX, startY, context);
+    if (colorsIdentical(targetColor, fillColor)) { return; }
 
-        // BASE CASES - if the selected pixel color != start color or == the fill color, return
-        if (pixelColor !== startColor) { return; }
-        if (pixelColor === fillColor) { return; }
+    // check we are actually filling a different color
+    if (!colorsMatch(targetColor, fillColor)) {
+    
+        const rangeSq = range * range;
 
-        
+        // let pixelStack = [ {x: x, y: y} ];
+        let queue = new Queue();
+        queue.push({x: x, y: y});
 
-        // update the selected pixel to the fill color
-        this.setPixelColor(startX, startY, fillColor, context);
+        // while (pixelStack.length > 0) {
 
-        // recursively call floodfill to the left, right, top, and bottom cooridinates respectively
-        this.floodFillRecurse(startX + 1, startY, startColor, fillColor, canvas, context);
-        this.floodFillRecurse(startX - 1, startY, startColor, fillColor, canvas, context);
-        this.floodFillRecurse(startX, startY + 1, startColor, fillColor, canvas, context);
-        this.floodFillRecurse(startX, startY - 1, startColor, fillColor, canvas, context);
-    }
+        while (!queue.isEmpty()) {
+            // let { x, y } = pixelStack.pop();
+            let { x, y } = queue.pop();
+            
+            const currentColor = getPixel(imageData, x, y);
 
-      
-    // OLD -- used in recursion
-    getPixelColor(x, y, context) {
-        // start at x, y and get data for a single 1x1 pixel
-        let pixel = context.getImageData(x, y, 1, 1).data;
-        let pixelColor = {
-            red: pixel[0],
-            green: pixel[1],
-            blue: pixel[2],
-            alpha: pixel[3]
-        }
-        return color.rgbToHex(pixelColor);
-    }
-
-    // OLD - used in recursion
-    setPixelColor(x, y, fillColor, context) {
-        context.fillStyle = fillColor;
-        context.fillRect(x, y, 1, 1);
-    }
-
-
-
-    // Iterative fill
-    floodFill(ctx, x, y, fillColor, range = 1) {
-
-        // read the pixels in the canvas
-        const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-        
-        // flags for if we visited a pixel already
-        const visited = new Uint8Array(imageData.width, imageData.height);
-        
-        // get the color we're filling
-        const targetColor = this.getPixel(imageData, x, y);
-
-        if (this.colorsIdentical(targetColor, fillColor)) { return; }
-
-        // check we are actually filling a different color
-        if (!this.colorsMatch(targetColor, fillColor)) {
-        
-            const rangeSq = range * range;
-            const pixelsToCheck = [x, y];
-            while (pixelsToCheck.length > 0) {
-                const y = pixelsToCheck.pop();
-                const x = pixelsToCheck.pop();
-                
-                const currentColor = this.getPixel(imageData, x, y);
-
-                if (!visited[y * imageData.width + x] &&
-                    this.colorsMatch(currentColor, targetColor, rangeSq)) {
-                    this.setPixel(imageData, x, y, fillColor);
-                    visited[y * imageData.width + x] = 1;  // mark we were here already
-                    pixelsToCheck.push(x + 1, y);
-                    pixelsToCheck.push(x - 1, y);
-                    pixelsToCheck.push(x, y + 1);
-                    pixelsToCheck.push(x, y - 1);
-                }
+            if (!visited[y * imageData.width + x] &&
+                colorsMatch(currentColor, targetColor, rangeSq)) {
+                setPixel(imageData, x, y, fillColor);
+                visited[y * imageData.width + x] = 1;  // mark we were here already
+                // pixelStack.push( { x: x + 1, y: y } );
+                // pixelStack.push( { x: x - 1, y: y } );
+                // pixelStack.push( { x: x, y: y + 1 } );
+                // pixelStack.push( { x: x, y: y - 1 } );
+                queue.push( { x: x + 1, y: y } );
+                queue.push( { x: x - 1, y: y } );
+                queue.push( { x: x, y: y + 1 } );
+                queue.push( { x: x, y: y - 1 } );
             }
-            ctx.putImageData(imageData, 0, 0);
         }
+        ctx.putImageData(imageData, 0, 0);
     }
+}
 
-    getPixel(imageData, x, y) {
-        if (x < 0 || y < 0 || x >= imageData.width || y >= imageData.height) {
-            return [-1, -1, -1, -1];  // impossible color
-        } else {
-            const offset = (y * imageData.width + x) * 4;
-            return imageData.data.slice(offset, offset + 4);
-        }
-    }
-      
-    setPixel(imageData, x, y, color) {
+
+function getPixel(imageData, x, y) {
+    // if the given x,y are outside of the canvas
+    if (x < 0 || y < 0 || x >= imageData.width || y >= imageData.height) {
+        return [-1, -1, -1, -1];
+    } else {
+        // i'm not 100% sure how the offset works
         const offset = (y * imageData.width + x) * 4;
-        imageData.data[offset + 0] = color[0];
-        imageData.data[offset + 1] = color[1];
-        imageData.data[offset + 2] = color[2];
-        imageData.data[offset + 3] = color[3];
+        return imageData.data.slice(offset, offset + 4);
     }
-      
-    colorsMatch(a, b, rangeSq) {
-        const dr = a[0] - b[0];
-        const dg = a[1] - b[1];
-        const db = a[2] - b[2];
-        const da = a[3] - b[3];
-        return dr * dr + dg * dg + db * db + da * da < rangeSq;
-    }
+}
+    
+// set the pixel at the given x,y coordinate to the given color
+function setPixel(imageData, x, y, color) {
+    const offset = (y * imageData.width + x) * 4;
+    imageData.data[offset + 0] = color[0];
+    imageData.data[offset + 1] = color[1];
+    imageData.data[offset + 2] = color[2];
+    imageData.data[offset + 3] = color[3];
+}
+    
+// returns true if the colors are similiar enough
+function colorsMatch(a, b, rangeSq) {
+    const dr = a[0] - b[0];
+    const dg = a[1] - b[1];
+    const db = a[2] - b[2];
+    const da = a[3] - b[3];
+    return dr * dr + dg * dg + db * db + da * da < rangeSq;
+}
 
-
-    colorsIdentical(a, b) {
-        return a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3];
-    }
+// returns true if the rgb arrays for both colors are the exact same
+function colorsIdentical(a, b) {
+    return a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3];
 }
